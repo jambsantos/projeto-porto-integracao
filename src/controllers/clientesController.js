ce11ad3432adeff214f614be22a49494876e3644
jessica-MBS <jessica.aparecida@gmail.com>
2020-11-07 23:22:14 -0300

    version 1.1

--- src/controllers/clientesController.js
+++ src/controllers/clientesController.js
@@ -1,7 +1,27 @@
-const getAll = (req, res) => {
-    console.log("getAll");
-      res.status(200).send("ok");
+const clientes = require('../models/clientes');
+
+const postCliente = (req, res) => {
+  let cliente = new clientes(req.body);
+  cliente.save(err => {
+    if(err) {
+      res.status(424).send({ message: err.message })
+    }
+      res.status(201).send({
+        status: 'TRUE',
+        message: 'Cliente incluído com sucesso'
+      });
+  });
 };
+
+const getAll = (req, res) => {
+  clientes.find((err,clientes)=>{
+    if(err) {
+      res.status(424).send({ message: err.message })
+    }
+      res.status(201).send(clientes)
+  });
+};
+
 
 const getCompradores = (req, res) => {
     console.log("getCompradores");
@@ -11,12 +31,8 @@ const getCompradores = (req, res) => {
 const getByCpf = (req, res) => {
     console.log("getByCpf");
       res.status(200).send("ok");
-};
+    };
 
-const postCliente = (req, res) => {
-    console.log("postCliente");
-      res.status(200).send("ok");
-};
 
 module.exports = {
     getAll,
--- src/routes/clientesRoute.js
+++ src/routes/clientesRoute.js
@@ -2,9 +2,9 @@ const express = require("express");
 const router = express.Router();
 const controller = require("../controllers/clientesController.js");
 
+router.post("/", controller.postCliente);
 router.get("/", controller.getAll);
 router.get("/compradores", controller.getCompradores);
 router.get("/:cpf", controller.getByCpf);
-router.post("/", controller.postCliente);
 
 module.exports = router;
